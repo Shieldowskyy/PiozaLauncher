e405305de888ce8166007afc20f0db3197a1ba17
Shieldziak <32707076+Shieldowskyy@users.noreply.github.com>
2025-10-19 02:51:52 +0200

    Optimized app size on android & removed sentry
    - Removed the Sentry plugin that caused issues with .gitignore and should be downloaded separately (it is also optional)
    - Optimized the package size on Android by 10%
    - Removed unused android permissions (testing required)

--- Plugins/Sentry/Source/Sentry/Private/SentryBeforeLogHandler.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2025 Sentry. All Rights Reserved.
-
-#include "SentryBeforeLogHandler.h"
-#include "SentryLog.h"
-
-USentryLog* USentryBeforeLogHandler::HandleBeforeLog_Implementation(USentryLog* LogData)
-{
-	return LogData;
-}
