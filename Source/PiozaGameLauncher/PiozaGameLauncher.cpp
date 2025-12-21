b01a50d9d7e9b2d0355edeee95be19e0768744cb
Shieldziak <32707076+Shieldowskyy@users.noreply.github.com>
2025-12-21 06:29:25 +0100

    Enhance graphics config, gc and command line handling
    Added D3D11 ES31 shader format and improved garbage collection settings in DefaultEngine.ini. Updated PiozaGameLauncher.cpp to automatically append -FeatureLevelES31 to the command line if not present, and improved logging. Updated W_MainLauncher1 for improved manual GC after downloading and unpacking.

--- Source/PiozaGameLauncher/PiozaGameLauncher.cpp
+++ Source/PiozaGameLauncher/PiozaGameLauncher.cpp
@@ -1,7 +1,6 @@
 // Pioza Launcher
 // Copyright (c) 2025 DashoGames
 // Licensed under the MIT License - see LICENSE file for details
-
 #include "PiozaGameLauncher.h"
 #include "Modules/ModuleManager.h"
 #include "Misc/CommandLine.h"
@@ -13,14 +12,28 @@ public:
     {
         FDefaultGameModuleImpl::StartupModule();
         
+#if !WITH_EDITOR
         FString CurrentCommandLine = FCommandLine::Get();
+        bool bModified = false;
+        
         if (!CurrentCommandLine.Contains(TEXT("AllowSoftwareRendering")))
         {
             CurrentCommandLine += TEXT(" -AllowSoftwareRendering");
-            FCommandLine::Set(*CurrentCommandLine);
-            
-            UE_LOG(LogTemp, Log, TEXT("Software rendering enabled automatically"));
+            bModified = true;
         }
+        
+        if (!CurrentCommandLine.Contains(TEXT("FeatureLevelES31")))
+        {
+            CurrentCommandLine += TEXT(" -FeatureLevelES31");
+            bModified = true;
+        }
+        
+        if (bModified)
+        {
+            FCommandLine::Set(*CurrentCommandLine);
+            UE_LOG(LogTemp, Log, TEXT("Command line updated: %s"), *CurrentCommandLine);
+        }
+#endif
     }
 };
 
